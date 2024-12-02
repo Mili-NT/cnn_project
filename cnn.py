c399a4da14b4cf9b0dacd9a633a745405fe0d38d
Mili <55.980.11sec@gmail.com>
2024-12-02 07:48:48 -0500

    Update

--- cnn.py
+++ cnn.py
@@ -27,33 +27,64 @@ class ImprovedCNN(nn.Module):
     def __init__(self):
         super(ImprovedCNN, self).__init__()
 
-        self.conv1 = nn.Sequential(
+        # First block
+        self.block1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)  # Downsample by 2
         )
-        self.conv2 = nn.Sequential(
+
+        # Second block
+        self.block2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)  # Downsample by 2
         )
-        self.conv3 = nn.Sequential(
+
+        # Third block
+        self.block3 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)  # Downsample by 2
         )
-        self.conv4 = nn.Sequential(
+
+        # Fourth block
+        self.block4 = nn.Sequential(
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)  # Downsample by 2
         )
 
+        # Fifth block
+        self.block5 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1))  # Global average pooling
+        )
+
+        # Fully connected layers
         self.fc1 = nn.Sequential(
-            nn.Linear(512 * 2 * 2, 512),
+            nn.Linear(512, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(),
             nn.Dropout(0.5)
@@ -67,11 +98,13 @@ class ImprovedCNN(nn.Module):
         self.fc3 = nn.Linear(256, 10)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = x.view(-1, 512 * 2 * 2)  # Flatten the tensor
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+
+        x = x.view(x.size(0), -1)  # Flatten the tensor
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
@@ -81,9 +114,9 @@ class ImprovedCNN(nn.Module):
 hyperparameters =  {
     'batch_size': 256,
     'learning_rate': 0.00029547769932519556,
-    'num_epochs': 300,
+    'num_epochs': 700,
     'gamma': 0.1342360529056426,
-    'step_size':30
+    'step_size': 15
 }
 # Configure logging
 logging.basicConfig(
